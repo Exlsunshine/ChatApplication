45877c9c8b86f675028bbdaa3f35913d5d2e4aec
sunshine <sunshine46582747@sina.com>
2015-03-18 10:20:34 +0800

    Add implementations to ClientUser.java.

--- Client code/MobileSocial/src/com/user/ClientUser.java
+++ Client code/MobileSocial/src/com/user/ClientUser.java
@@ -11,6 +11,7 @@ public class ClientUser extends AbstractUser
 {
 	private String wsNamespace = "http://network.com";
 	private String wsEndpoint = "http://192.168.95.1:8080/WebServiceProject/services/Main";
+	private WebServiceAPI wsAPI = new WebServiceAPI(wsNamespace, wsEndpoint);
 	private Context context;
 	
 	/****************************		以下是网络操作相关内容		****************************/
@@ -25,8 +26,6 @@ public class ClientUser extends AbstractUser
 	 */
 	public void setPassword(String password)
 	{
-		WebServiceAPI wsAPI = new WebServiceAPI(wsNamespace, wsEndpoint);
-		
 		String [] params = new String[2];
 		Object [] vlaues = new Object[2];
 		params[0] = "userID";
@@ -43,28 +42,72 @@ public class ClientUser extends AbstractUser
 	 * 设置用户昵称
 	 * @param nickName 昵称
 	 */
-	public void setNickName(String nickName) {
+	public void setNickName(String nickName) 
+	{
+		String [] params = new String[2];
+		Object [] vlaues = new Object[2];
+		params[0] = "userID";
+		params[1] = "nickName";
+		vlaues[0] = this.id;
+		vlaues[1] = nickName;
+		
+		Object ret = wsAPI.callFuntion("setNickName", params, vlaues);
+		
+		Log.e("______", ret.toString());
 	}
 	
 	/**
 	 * 设置用户邮箱
 	 * @param email 邮箱
 	 */
-	public void setEmail(String email) {
+	public void setEmail(String email) 
+	{
+		String [] params = new String[2];
+		Object [] vlaues = new Object[2];
+		params[0] = "userID";
+		params[1] = "email";
+		vlaues[0] = this.id;
+		vlaues[1] = email;
+		
+		Object ret = wsAPI.callFuntion("setEmail", params, vlaues);
+		
+		Log.e("______", ret.toString());
 	}
 	
 	/**
 	 * 设置用户性别
 	 * @param sex 性别
 	 */
-	public void setSex(String sex) {
+	public void setSex(String sex)
+	{
+		String [] params = new String[2];
+		Object [] vlaues = new Object[2];
+		params[0] = "userID";
+		params[1] = "sex";
+		vlaues[0] = this.id;
+		vlaues[1] = sex;
+		
+		Object ret = wsAPI.callFuntion("setSex", params, vlaues);
+		
+		Log.e("______", ret.toString());
 	}
 	
 	/**
 	 * 设置用户生日
 	 * @param birthday 生日
 	 */
-	public void setBirthday(String birthday) {
+	public void setBirthday(String birthday) 
+	{
+		String [] params = new String[2];
+		Object [] vlaues = new Object[2];
+		params[0] = "userID";
+		params[1] = "birthday";
+		vlaues[0] = this.id;
+		vlaues[1] = birthday;
+		
+		Object ret = wsAPI.callFuntion("setBirthday", params, vlaues);
+		
+		Log.e("______", ret.toString());
 	}
 	
 	/**
@@ -78,14 +121,36 @@ public class ClientUser extends AbstractUser
 	 * 设置用户家乡
 	 * @param hometown 家乡
 	 */
-	public void setHometown(String hometown) {
+	public void setHometown(String hometown)
+	{
+		String [] params = new String[2];
+		Object [] vlaues = new Object[2];
+		params[0] = "userID";
+		params[1] = "hometown";
+		vlaues[0] = this.id;
+		vlaues[1] = hometown;
+		
+		Object ret = wsAPI.callFuntion("setHometown", params, vlaues);
+		
+		Log.e("______", ret.toString());
 	}
 	
 	/**
 	 * 设置用户手机号
 	 * @param phoneNumber 手机号
 	 */
-	public void setPhoneNumber(String phoneNumber) {
+	public void setPhoneNumber(String phoneNumber)
+	{
+		String [] params = new String[2];
+		Object [] vlaues = new Object[2];
+		params[0] = "userID";
+		params[1] = "phoneNumber";
+		vlaues[0] = this.id;
+		vlaues[1] = phoneNumber;
+		
+		Object ret = wsAPI.callFuntion("setPhoneNumber", params, vlaues);
+		
+		Log.e("______", ret.toString());
 	}
 
 	/**
@@ -129,7 +194,20 @@ public class ClientUser extends AbstractUser
 	 * @param other 待设置的目标用户
 	 * @param alias 别名
 	 */
-	public void setAlias(ClientUser other, String alias) {
+	public void setAlias(ClientUser other, String alias) 
+	{
+		String [] params = new String[3];
+		Object [] vlaues = new Object[3];
+		params[0] = "userID";
+		params[1] = "anotherUserID";
+		params[3] = "alias";
+		vlaues[0] = this.id;
+		vlaues[1] = other.getID();
+		vlaues[1] = alias;
+		
+		Object ret = wsAPI.callFuntion("setAlias", params, vlaues);
+		
+		Log.e("______", ret.toString());
 	}
 	
 	/**
@@ -137,28 +215,74 @@ public class ClientUser extends AbstractUser
 	 * @param other 待设置的目标用户
 	 * @param groupName 分组名称
 	 */
-	public void moveFriendToGroup(ClientUser other, String groupName) {
+	public void moveFriendToGroup(ClientUser other, String groupName)
+	{
+		String [] params = new String[3];
+		Object [] vlaues = new Object[3];
+		params[0] = "userID";
+		params[1] = "anotherUserID";
+		params[3] = "groupName";
+		vlaues[0] = this.id;
+		vlaues[1] = other.getID();
+		vlaues[1] = groupName;
+		
+		Object ret = wsAPI.callFuntion("moveFriendToGroup", params, vlaues);
+		
+		Log.e("______", ret.toString());
 	}
 	
 	/**
 	 * 将other这个用户设为星标用户
 	 * @param other 待设置的目标用户
 	 */
-	public void markCloseFriend(ClientUser other) {
+	public void markCloseFriend(ClientUser other)
+	{
+		String [] params = new String[2];
+		Object [] vlaues = new Object[2];
+		params[0] = "userID";
+		params[1] = "anotherUserID";
+		vlaues[0] = this.id;
+		vlaues[1] = other.getID();
+		
+		Object ret = wsAPI.callFuntion("markCloseFriend", params, vlaues);
+		
+		Log.e("______", ret.toString());
 	}
 	
 	/**
 	 * 将other这个用户加入黑名单
 	 * @param other 待设置的目标用户
 	 */
-	public void blockUser(ClientUser other) {
+	public void blockUser(ClientUser other)
+	{
+		String [] params = new String[2];
+		Object [] vlaues = new Object[2];
+		params[0] = "userID";
+		params[1] = "anotherUserID";
+		vlaues[0] = this.id;
+		vlaues[1] = other.getID();
+		
+		Object ret = wsAPI.callFuntion("blockUser", params, vlaues);
+		
+		Log.e("______", ret.toString());
 	}
 	
 	/**
 	 * 将other这个用户解除好友关系
 	 * @param other 待设置的目标用户
 	 */
-	public void deleteUser(ClientUser other){
+	public void deleteUser(ClientUser other)
+	{
+		String [] params = new String[2];
+		Object [] vlaues = new Object[2];
+		params[0] = "userID";
+		params[1] = "anotherUserID";
+		vlaues[0] = this.id;
+		vlaues[1] = other.getID();
+		
+		Object ret = wsAPI.callFuntion("deleteUser", params, vlaues);
+		
+		Log.e("______", ret.toString());
 	}
 	
 	/**
