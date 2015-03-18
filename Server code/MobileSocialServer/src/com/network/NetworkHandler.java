2ee903f4b0f32524842d2e35cdbebf55fc0aecfb
sunshine <sunshine46582747@sina.com>
2015-03-18 10:39:23 +0800

    Init server-end project code.

---
+++ Server code/MobileSocialServer/src/com/network/NetworkHandler.java
@@ -0,0 +1,57 @@
+package com.network;
+
+public class NetworkHandler
+{
+	public void setPassword(int userID, String password)
+	{
+	}
+	
+	public void setNickName(int userID, String nickName) 
+	{
+	}
+	
+	public void setEmail(int userID, String email) 
+	{
+	}
+	
+	public void setSex(int userID, String sex) 
+	{
+	}
+	
+	public void setBirthday(int userID, String birthday) 
+	{
+	}
+	
+	public void setHometown(int userID, String hometown) 
+	{
+	}
+	
+	public void setPhoneNumber(int userID, String phoneNumber) 
+	{
+	}
+	
+	public void setAlias(int userID, int anotherUserID, String alias)
+	{
+		
+	}
+	
+	public void moveFriendToGroup(int userID, int anotherUserID, String groupName)
+	{
+		
+	}
+	
+	public void markCloseFriend(int userID, int anotherUserID)
+	{
+		
+	}
+	
+	public void blockUser(int userID, int anotherUserID)
+	{
+		
+	}
+	
+	public void deleteUser(int userID, int anotherUserID)
+	{
+		
+	}
+}
