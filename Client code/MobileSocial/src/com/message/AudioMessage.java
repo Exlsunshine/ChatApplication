f0b59a374b47ca0d3f341365b36874244e4ed733
EXLsunshine <sunshine46582747@sina.com>
2015-03-22 20:37:38 +0800

    Add getAudioPath() function to AudioMessage.java.

--- Client code/MobileSocial/src/com/message/AudioMessage.java
+++ Client code/MobileSocial/src/com/message/AudioMessage.java
@@ -1,6 +1,7 @@
 package com.message;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 import com.configs.ConstantValues;
@@ -16,9 +17,11 @@ import android.media.MediaPlayer;
  */
 public class AudioMessage extends AbstractMessage
 {
+	private static String AUDIO_CACHE_DIR = "mnt/sdcard/MobileSocial/audio/";
 	private MediaPlayer mPlayer;
 	private Context context;
 	private byte [] audio;
+	private String audioPath = null;
 	
 	public AudioMessage(int msgID, int fromID, int toID, byte [] content, String date, boolean isRead)
 	{
@@ -39,6 +42,40 @@ public class AudioMessage extends AbstractMessage
 		this.type = ConstantValues.InstructionCode.MESSAGE_TYPE_AUDIO;
 		this.isRead = isRead;
 		this.audio = content;
+	}
+	
+	/**
+	 * 返回音频文件路径
+	 * @return null 表示文件获取有误<br>
+	 * 非null 表示获取文件成功
+	 */
+	public String getAudioPath()
+	{
+		if (audioPath != null)
+		{
+			File audioCacheFile = new File(audioPath);
+			if (audioCacheFile.isFile() && audioCacheFile.exists())
+				return audioPath;
+		}
+		
+		try {
+			generateAudioCache();
+		} catch (IOException e) {
+			audioPath = null;
+			e.printStackTrace();
+		}
+		return audioPath;
+	}
+	
+	private void generateAudioCache() throws IOException
+	{
+		// create temp file that will hold byte array
+        File tempMp3 = File.createTempFile("audio_" + String.valueOf(fromUserID)
+        		+ "_" + String.valueOf(toUserID) + getDate(), "amr", new File(AUDIO_CACHE_DIR));
+        audioPath = AUDIO_CACHE_DIR + "audio_" + String.valueOf(fromUserID) + "_" + String.valueOf(toUserID) + getDate() + ".amr";
+        FileOutputStream fos = new FileOutputStream(tempMp3);
+        fos.write(audio);
+        fos.close();
 	}
 	
 	/**
