c34fbb8e2ad23940b676c06d5315c89997123b21
foreverhyjal <fml_lj@163.com>
2015-05-25 17:29:44 +0800

    Bug Fix : openfire server name

--- Server code/MobileSocialServer/src/com/commonapi/ConstantValues.java
+++ Server code/MobileSocialServer/src/com/commonapi/ConstantValues.java
@@ -55,8 +55,8 @@ public class ConstantValues {
 	//	public static final String TORNADO_SERVER_IP = "172.18.7.189";
 		public static final String TORNADO_SERVER_PORT = "8887";
 		
-	//	public static final String DATABASE_SERVER_IP = "101.200.230.173";
-		public static final String DATABASE_SERVER_IP = "127.0.0.1";
+		public static final String DATABASE_SERVER_IP = "101.200.230.173";
+	//	public static final String DATABASE_SERVER_IP = "127.0.0.1";
 		public static final String DATABASE_SERVER_PORT = "1433";
 	}
 }
