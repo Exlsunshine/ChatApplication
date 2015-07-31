1b2d20d809ec23c78c474eee5c15c501c8ca5b11
foreverhyjal <fml_lj@163.com>
2015-07-31 10:41:01 +0800

    Build web demo

--- MaintainServer/Display/views.py
+++ MaintainServer/Display/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def line(request):
+    return render(request, "line.html")
+
+def column(request):
+    sexDict = {'male' : 30.34, 'female' : 40.32}
+    return render(request, "column.html", {'sexDict' : sexDict})
--- MaintainServer/MaintainServer/urls.py
+++ MaintainServer/MaintainServer/urls.py
@@ -18,4 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^column$', 'Display.views.column'),
+    url(r'^line$', 'Display.views.line'),
 ]
